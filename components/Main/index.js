f28952d1cf5ea3c953a1652df3ec688eee9bc7ae
udayanshevade <udayan.shevade@gmail.com>
2018-01-26 12:52:15 -0800

    style main container

--- components/Main/index.js
+++ components/Main/index.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 import Filter from '../Filter';
 import Decks from '../Decks';
 
 const Main = ({ decks, isLoading }) => (
-  <View style={{ flex: 1, justifyContent: 'center', alignSelf: 'center' }}>
+  <View style={styles.main}>
     <Filter />
     <Decks />
   </View>
 );
+
+const styles = StyleSheet.create({
+  main: {
+    flex: 1,
+    alignItems: 'center',
+    width: '100%',
+  },
+});
 
 export default Main;
